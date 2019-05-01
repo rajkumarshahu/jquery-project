41bd97114eacde0b4c04ba07e7e83104bc739033
Jyoti Adhikari <jyotiad18@gmail.com>
2019-05-01 23:16:11 +0200

    jquery connection with sign in and sign up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,39 +2,78 @@
 import $ from 'jquery';
 
 const mockUrl = 'http://5cc75aa0ae1431001472e41b.mockapi.io/api/v1/';
+const signupForm = $('#signup-form');
+const signinForm = $('#signin-form');
+let promise = null;
 
+/* ########### SignUp Form  ############## */
 
+const clearSignUpForm = () => {
+	$("#fullName").val('');
+	$("#email").val('');
+	$("#userName").val('');
+	$("#password").val('');
+};
 
-$('#signin-form').submit((event) => {
-	console.log(mockUrl);
-	alert(mockUrl);
-	e.preventDefault();
+const clearSignInForm = () => {
 
-});
+};
 
-/*
-let form = document.getElementById('user-form');
-
-form.addEventListener('submit', event => {
-
-	let user = form.elements['user'];
-	let avatarFile = form.elements['avatar-file'];
-
-	let posting = {
-		user: user.value,
-		avatarFile: avatarFile.value
+$("#submit-button").click((e) => {
+   
+	const fullName = $("#fullName").val();
+	const email = $("#email").val();
+	const userName= $("#userName").val();
+	const password = $("#password").val();
+	
+	const user = {
+		fullName: fullName,
+		email: email,
+		userName: userName,
+		password: password
 	};
-	
-	let promise = $.post(
-		"http://5cc75aa0ae1431001472e41b.mockapi.io/api/v1/products", posting
-	);
+
+	promise = $.post(mockUrl + "jusers", user);
+
 	promise.then(
-	    data => console.log('success: ', data),
-	    error => console.log('error: ', error)
+		() => {
+			clearSignUpForm();
+			alert('User create sucessfully 1');
+		},
+		error => console.log('error: ', error)
 	);
-	
 
-	event.preventDefault();
-
+	e.preventDefault();
 });
-*/
+
+/* ############# End of SignUp Form  ############## */
+
+/* ############ SignIn Form ###################### */
+
+$("#login-button").click((e) => {
+	const userName = $("#userNames").val();
+	const password = $("#passwords").val();
+
+	promise = $.get(mockUrl + "jusers");
+
+	promise.then(
+		(data) => {
+			//* filter
+			const result = data.filter(x => x.userName == userName && x.password == password);
+			if (result.length > 0)
+			{
+				alert('Sucesfully Login !!');
+			} else {
+				alert('Username && Password is incorrect !!');
+				/* clearfunction */
+			}
+			
+		},
+		error => console.log('error: ', error)
+	);
+
+	e.preventDefault();
+});
+
+
+/* ############ End SignIn Form ###################### */
